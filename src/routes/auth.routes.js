159de3b975f2e46d027f99b39294156f1eedf159
andrawicaksono <andrawicaksono14@gmail.com>
2025-02-25 00:17:33 +0700

    feat: add input validation for login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { authController } = require("../container");
+const { validateInput } = require("../middlewares/input.middleware");
+const { loginSchema } = require("../validators/auth.validator");
 
-router.post("/login", authController.login);
+router.post("/login", validateInput(loginSchema), authController.login);
 
 module.exports = router;
